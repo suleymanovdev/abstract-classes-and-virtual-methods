dc24e8ab9f731d4d1d07c44f1f37188c354f7cbe
SuleymanovDev®™ <97979588+suleymanovdev@users.noreply.github.com>
2022-06-25 22:41:32 +0400

    banner_class.h

---
+++ banner_class.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+void trapezoid_banner()
+{
+	cout << "    ******    " << endl;
+	cout << "   *      *   " << endl;
+	cout << "  *        *  " << endl;
+	cout << " *          * " << endl;
+	cout << "*            *" << endl;
+	cout << "**************" << endl;
+}
+
+void circle_banner()
+{
+	cout << "   ********   " << endl;
+	cout << " *          * " << endl;
+	cout << "*            *" << endl;
+	cout << "*            *" << endl;
+	cout << " *          * " << endl;
+	cout << "   ********   " << endl;
+}
+
+void rectangle_banner()
+{
+	cout << "**************" << endl;
+	cout << "*            *" << endl;
+	cout << "*            *" << endl;
+	cout << "*            *" << endl;
+	cout << "*            *" << endl;
+	cout << "**************" << endl;
+}
+
+void right_triangle_banner()
+{
+	cout << "**     " << endl;
+	cout << "* *    " << endl;
+	cout << "*  *   " << endl;
+	cout << "*   *  " << endl;
+	cout << "*    * " << endl;
+	cout << "*******" << endl;
+}
